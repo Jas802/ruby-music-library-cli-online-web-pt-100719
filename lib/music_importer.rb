f03b757c716d7dc3f7530c402247b11a496d44c5
Jas Spencer <jas.spencer414@gmail.com>
2019-11-18 18:36:25 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -6,7 +6,7 @@ def initialize(path)
 end
 
 def files
-  @files = ||= Dir.glob("#{path}/*.mp3").collect{ |file| file.gsub("#{path}/", "")}
+  @files ||= Dir.glob("#{path}/*.mp3").collect{ |file| file.gsub("#{path}/", "")}
 end
 
 def import
